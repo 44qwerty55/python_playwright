71330cc46dfa6b1013dd22ab2941e0783d055f36
44qwerty55 <73277467+44qwerty55@users.noreply.github.com>
2025-02-19 19:25:00 +0300

    add negative test

---
+++ tests/saucedemo/test_check_negative.py
@@ -0,0 +1,26 @@
+import pytest
+from playwright.sync_api import expect
+
+test_data = [
+    ("locked_out_user", "secret_sauce", "Epic sadface: Sorry, this user has been locked out."),
+    ("visual_user", "another_value", "Epic sadface: Username and password do not match any user in this service"),
+    ("visual_user", "", "Epic sadface: Password is required")
+]
+
+@pytest.mark.parametrize("user, password, error_message", test_data)
+def test_login(open_base_page, user, password, error_message):
+    page = open_base_page
+    select_user_name_element = page.locator('[id="user-name"]')
+    select_user_name_element.fill(user)
+    page.wait_for_timeout(1000)
+    select_user_pass_element = page.locator('[id="password"]')
+    select_user_pass_element.fill(password)
+    button = page.get_by_role('button', name='Login')
+    button.click()
+
+    page.wait_for_url('https://www.saucedemo.com/')
+    error_button = page.locator('h3[data-test="error"]')
+    expect(error_button).to_be_visible()
+    expect(error_button).to_have_text(error_message)
+
+
