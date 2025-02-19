b493f06c672010d73a9d07bd17d3081f84cb25c0
44qwerty55 <73277467+44qwerty55@users.noreply.github.com>
2025-02-19 19:34:21 +0300

    small fix

--- tests/saucedemo/test_check_negative.py
+++ tests/saucedemo/test_check_negative.py
@@ -4,7 +4,8 @@ from playwright.sync_api import expect
 test_data = [
     ("locked_out_user", "secret_sauce", "Epic sadface: Sorry, this user has been locked out."),
     ("visual_user", "another_value", "Epic sadface: Username and password do not match any user in this service"),
-    ("visual_user", "", "Epic sadface: Password is required")
+    ("visual_user", "", "Epic sadface: Password is required"),
+    ("", "secret_sauce", "Epic sadface: Username is required"),
 ]
 
 @pytest.mark.parametrize("user, password, error_message", test_data)
@@ -12,11 +13,11 @@ def test_login(open_base_page, user, password, error_message):
     page = open_base_page
     select_user_name_element = page.locator('[id="user-name"]')
     select_user_name_element.fill(user)
-    page.wait_for_timeout(1000)
     select_user_pass_element = page.locator('[id="password"]')
     select_user_pass_element.fill(password)
     button = page.get_by_role('button', name='Login')
     button.click()
+    page.wait_for_timeout(1000)
 
     page.wait_for_url(page.url)
     error_button = page.locator('h3[data-test="error"]')
