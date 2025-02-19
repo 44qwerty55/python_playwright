027a9ef2c9b3c6fe17633c460142209326387b42
44qwerty55 <73277467+44qwerty55@users.noreply.github.com>
2025-02-19 19:13:30 +0300

    add saucedemo

--- tests/saucedemo/test_check.py
+++ tests/saucedemo/test_check.py
@@ -2,16 +2,18 @@ import pytest
 from playwright.sync_api import expect
 
 test_data = [
-    ("standard_user", "secret_sauce", True),
-    ("locked_out_user", "secret_sauce", False),
-    ("problem_user", "secret_sauce", True),
-    ("performance_glitch_user", "secret_sauce", True),
-    ("error_user", "secret_sauce", True),
-    ("visual_user", "secret_sauce", True),
+    ("standard_user", "secret_sauce", True, None),
+    ("locked_out_user", "secret_sauce", False, "Epic sadface: Sorry, this user has been locked out."),
+    ("problem_user", "secret_sauce", True, None),
+    ("performance_glitch_user", "secret_sauce", True, None),
+    ("error_user", "secret_sauce", True, None),
+    ("visual_user", "secret_sauce", True, None),
+    ("visual_user", "another_value", False, "Epic sadface: Username and password do not match any user in this service"),
+    ("visual_user", "", False, "Epic sadface: Password is required")
 ]
 
-@pytest.mark.parametrize("user, password, result", test_data)
-def test_write_you_name_text(open_base_page, user, password, result):
+@pytest.mark.parametrize("user, password, result, error_message", test_data)
+def test_write_you_name_text(open_base_page, user, password, result, error_message):
     page = open_base_page
     select_user_name_element = page.locator('[id="user-name"]')
     select_user_name_element.fill(user)
@@ -23,6 +25,8 @@ def test_write_you_name_text(open_base_page, user, password, result):
     if result:
         page.wait_for_url('https://www.saucedemo.com/inventory.html')
     else:
-        expect(page.locator('button.error-button')).to_be_visible()
+        error_button = page.locator('h3[data-test="error"]')
+        expect(error_button).to_be_visible()
+        expect(error_button).to_have_text(error_message)
 
 
