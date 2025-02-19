2f1694c9766279690a7eacf13b7614d1aa6b3ba7
44qwerty55 <73277467+44qwerty55@users.noreply.github.com>
2025-02-19 21:32:46 +0300

    add login page , refactoring tests

--- tests/saucedemo/conftest.py
+++ tests/saucedemo/conftest.py
@@ -1,11 +1,17 @@
 import pytest
 from playwright.sync_api import Page
 
+from pages.login_page import LoginPage
+
 
 @pytest.fixture
 def open_base_page(page: Page):
     page.set_viewport_size({"width": 1600, "height": 1200})
     page.goto('https://www.saucedemo.com/')
     return page
+
+@pytest.fixture
+def login_page(page: Page):
+    return LoginPage(page)
 
 
