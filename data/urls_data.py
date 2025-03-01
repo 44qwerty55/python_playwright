901eac398547fe1f99b693c8346ae306f6617c41
44qwerty55 <73277467+44qwerty55@users.noreply.github.com>
2025-03-01 19:12:03 +0300

    small fix

--- data/urls_data.py
+++ data/urls_data.py
@@ -1,3 +1,4 @@
 # Urls
 AUTHN_PAGE_URL = "https://www.saucedemo.com/"
 INVENTORY_PAGE_URL = "https://www.saucedemo.com/inventory.html"
+TEST_CAFE_PAGE = "https://devexpress.github.io/testcafe/example/"
--- tests/devexpress/conftest.py
+++ tests/devexpress/conftest.py
@@ -1,10 +1,12 @@
 import pytest
 from playwright.sync_api import Page
 
+from data.urls_data import TEST_CAFE_PAGE
+
 
 @pytest.fixture
 def open_base_page(page: Page):
     page.set_viewport_size({"width": 1600, "height": 1200})
-    page.goto('https://devexpress.github.io/testcafe/example/')
+    page.goto(TEST_CAFE_PAGE)
     return page
 
